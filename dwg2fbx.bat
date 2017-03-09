6fce723903e195deeedf808565f875fb807825ac
yanagiragi <qaz90473@gmail.com>
2017-03-10 04:32:27 +0800

    Changle to Single line For Convince Excuting

--- dwg2fbx.bat
+++ dwg2fbx.bat
@@ -1,2 +1,1 @@
-cd "C:\Program Files\Autodesk\3ds Max 2016"
- .\3dsmax.exe -q -mi -silent -U MAXScript C:\Users\%USERNAME%\Desktop\ypMaxScript.ms
+"C:\Program Files\Autodesk\3ds Max 2016\3dsmax.exe" -q -mi -silent -U MAXScript C:\Users\%USERNAME%\Desktop\ypMaxScript.ms
